0bd1a39939830b237354a8471c4d132bba3f7d58
Owen Wang <wangfowen@gmail.com>
2019-12-05 15:48:28 -0500

    ruben's dynamic menu code squashed

---
+++ test/unit/app/utils/ussd/kenya/test_kenya_ussd_processor.py
@@ -0,0 +1,43 @@
+import pytest
+from functools import partial
+from server.utils.ussd.kenya_ussd_processor import KenyaUssdProcessor
+
+from helpers.user import UserFactory, TransferUsageFactory
+from helpers.ussd_session import UssdSessionFactory, UssdMenuFactory
+
+standard_user = partial(UserFactory)
+
+
+def mock_get_most_relevant_transfer_usage():
+    transfer_usage_1 = partial(TransferUsageFactory)
+    transfer_usage_1.id = 1
+    transfer_usage_1.name = 'Food'
+    transfer_usage_1.translations = {'en': 'Food', 'sw': 'Chakula'}
+    transfer_usage_2 = partial(TransferUsageFactory)
+    transfer_usage_2.id = 2
+    transfer_usage_2.translations = {'en': 'Education', 'sw': 'Elimu'}
+    transfer_usage_2.name = 'Education'
+    list_of_transfer_usage = [transfer_usage_1, transfer_usage_2]
+    return list_of_transfer_usage
+
+
+@pytest.mark.parametrize("menu_name, language, expected", [
+    ("send_token_reason", "en", '\n1. Food\n2. Education'),
+    ("send_token_reason", "sw", '\n1. Chakula\n2. Elimu'),
+    ("send_token_reason", None, '\n1. Food\n2. Education'),
+    ("directory_listing", "en", '\n1. Food\n2. Education'),
+    ("directory_listing", "sw", '\n1. Chakula\n2. Elimu'),
+    ("directory_listing", None, '\n1. Food\n2. Education'),
+])
+def test_custom_display_text(mocker, test_client, init_database, menu_name, language, expected):
+    start_state = UssdSessionFactory()
+    user = standard_user()
+    user.preferred_language = language
+
+    type(user).get_most_relevant_transfer_usage = mocker.PropertyMock(
+        return_value=mock_get_most_relevant_transfer_usage)
+    menu = UssdMenuFactory(name=menu_name, display_key="ussd.kenya.{}".format(menu_name))
+    resulting_menu = KenyaUssdProcessor.custom_display_text(
+        menu, start_state, user)
+
+    assert expected in resulting_menu
